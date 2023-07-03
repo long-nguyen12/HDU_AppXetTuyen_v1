f08d5e859d4504374eb232ad270cb9f5a47ecb4c
Cuongyd196 <cuongyd196@gmail.com>
2023-07-03 16:24:40 +0700

    #Update LienCapTieuHoc

---
+++ Controllers/LienCapTieuHocsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HDU_AppXetTuyen.Models;
+
+namespace HDU_AppXetTuyen.Controllers
+{
+    public class LienCapTieuHocsController : Controller
+    {
+        private DbConnecttion db = new DbConnecttion();
+
+        // GET: LienCapTieuHocs
+        public ActionResult Index()
+        {
+            return View(db.LienCapTieuHocs.ToList());
+        }
+
+        // GET: LienCapTieuHocs/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LienCapTieuHoc lienCapTieuHoc = db.LienCapTieuHocs.Find(id);
+            if (lienCapTieuHoc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lienCapTieuHoc);
+        }
+
+        // GET: LienCapTieuHocs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LienCapTieuHocs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "HocSinh_ID,HocSinh_DinhDanh,HocSinh_HoTen,HocSinh_GioiTinh,HocSinh_NgaySinh,HocSinh_NoiSinh,HocSinh_Email,HocSinh_NoiCuTru,HocSinh_TruongMN,HocSinh_UuTien,HocSinh_ThongTinCha,HocSinh_NgheNghiepCha,HocSinh_DienThoaiCha,HocSinh_ThongTinMe,HocSinh_NgheNghiepMe,HocSinh_DienThoaiMe,HocSinh_MinhChungMN,HocSinh_MinhChungGiayKS,HocSinh_MinhChungMaDinhDanh,HocSinh_GiayUuTien,HocSinh_XacNhanLePhi,HocSinh_TrangThai,HocSinh_GhiChu")] LienCapTieuHoc lienCapTieuHoc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.LienCapTieuHocs.Add(lienCapTieuHoc);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(lienCapTieuHoc);
+        }
+
+        // GET: LienCapTieuHocs/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LienCapTieuHoc lienCapTieuHoc = db.LienCapTieuHocs.Find(id);
+            if (lienCapTieuHoc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lienCapTieuHoc);
+        }
+
+        // POST: LienCapTieuHocs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "HocSinh_ID,HocSinh_DinhDanh,HocSinh_HoTen,HocSinh_GioiTinh,HocSinh_NgaySinh,HocSinh_NoiSinh,HocSinh_Email,HocSinh_NoiCuTru,HocSinh_TruongMN,HocSinh_UuTien,HocSinh_ThongTinCha,HocSinh_NgheNghiepCha,HocSinh_DienThoaiCha,HocSinh_ThongTinMe,HocSinh_NgheNghiepMe,HocSinh_DienThoaiMe,HocSinh_MinhChungMN,HocSinh_MinhChungGiayKS,HocSinh_MinhChungMaDinhDanh,HocSinh_GiayUuTien,HocSinh_XacNhanLePhi,HocSinh_TrangThai,HocSinh_GhiChu")] LienCapTieuHoc lienCapTieuHoc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lienCapTieuHoc).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(lienCapTieuHoc);
+        }
+
+        // GET: LienCapTieuHocs/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LienCapTieuHoc lienCapTieuHoc = db.LienCapTieuHocs.Find(id);
+            if (lienCapTieuHoc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lienCapTieuHoc);
+        }
+
+        // POST: LienCapTieuHocs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            LienCapTieuHoc lienCapTieuHoc = db.LienCapTieuHocs.Find(id);
+            db.LienCapTieuHocs.Remove(lienCapTieuHoc);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Models/LienCapTieuHoc.cs
+++ Models/LienCapTieuHoc.cs
@@ -1,4 +1,4 @@
-namespace HDU_AppXetTuyen.Models
+﻿namespace HDU_AppXetTuyen.Models
 {
     using System;
     using System.Collections.Generic;
@@ -12,66 +12,88 @@ namespace HDU_AppXetTuyen.Models
         [Key]
         public long HocSinh_ID { get; set; }
 
+        [Display(Name = "Mã định danh cá nhân")]
         [StringLength(100)]
         public string HocSinh_DinhDanh { get; set; }
 
+        [Display(Name = "Họ và tên")]
         [StringLength(100)]
         public string HocSinh_HoTen { get; set; }
 
+        [Display(Name = "Giới tính")]
         public int? HocSinh_GioiTinh { get; set; }
 
+        [Display(Name = "Ngày sinh")]
         [StringLength(20)]
         public string HocSinh_NgaySinh { get; set; }
 
+        [Display(Name = "Nơi sinh")]
         [StringLength(500)]
         public string HocSinh_NoiSinh { get; set; }
 
+        [Display(Name = "Email")]
         [StringLength(100)]
         public string HocSinh_Email { get; set; }
 
+        [Display(Name = "Nơi cư trú")]
         [StringLength(500)]
         public string HocSinh_NoiCuTru { get; set; }
 
+        [Display(Name = "Đã hoàn thành chương trình mẫu giáo tại trường")]
         [StringLength(500)]
         public string HocSinh_TruongMN { get; set; }
 
+        [Display(Name = "Thuộc diện ưu tiên")]
         [StringLength(100)]
         public string HocSinh_UuTien { get; set; }
 
+        [Display(Name = "Họ tên cha")]
         [StringLength(4000)]
         public string HocSinh_ThongTinCha { get; set; }
 
+        [Display(Name = "Nghề nghiệp cha")]
         [StringLength(4000)]
         public string HocSinh_NgheNghiepCha { get; set; }
 
+        [Display(Name = "Điện thoại cha")]
         [StringLength(20)]
         public string HocSinh_DienThoaiCha { get; set; }
 
+        [Display(Name = "Họ tên mẹ")]
         [StringLength(4000)]
         public string HocSinh_ThongTinMe { get; set; }
 
+        [Display(Name = "Nghề nghiệp mẹ")]
         [StringLength(4000)]
         public string HocSinh_NgheNghiepMe { get; set; }
 
+        [Display(Name = "Điện thoại mẹ")]
         [StringLength(20)]
         public string HocSinh_DienThoaiMe { get; set; }
 
+        [Display(Name = "Giấy chứng nhận hoàn thành chương trình giáo dục mầm non")]
         [StringLength(4000)]
         public string HocSinh_MinhChungMN { get; set; }
 
+        [Display(Name = "Giấy khai sinh")]
         [StringLength(4000)]
         public string HocSinh_MinhChungGiayKS { get; set; }
 
+        [Display(Name = "Mã định danh cá nhân")]
         [StringLength(4000)]
         public string HocSinh_MinhChungMaDinhDanh { get; set; }
 
+        [Display(Name = "Giấy chứng nhận ưu tiên")]
         [StringLength(4000)]
         public string HocSinh_GiayUuTien { get; set; }
 
+        [Display(Name = "Xác nhận lệ phí")]
         public int? HocSinh_XacNhanLePhi { get; set; }
 
+        [Display(Name = "Trạng thái")]
         public int? HocSinh_TrangThai { get; set; }
 
+        [Display(Name = "Ghi chú")]
         [StringLength(4000)]
         public string HocSinh_GhiChu { get; set; }
     }
