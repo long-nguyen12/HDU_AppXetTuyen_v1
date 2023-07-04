9fb1ae59a3fb0002ffea74ccc052888d8f902649
Cuongyd196 <cuongyd196@gmail.com>
2023-07-05 03:55:01 +0700

    #update  dev-cuong liencap

--- Controllers/LienCapTieuHocsController.cs
+++ Controllers/LienCapTieuHocsController.cs
@@ -56,13 +56,16 @@ namespace HDU_AppXetTuyen.Controllers
            IEnumerable<HttpPostedFileBase> HocSinh_MinhChungLePhi)
 
         {
+            // Tạo chuỗi kí tự ngẫu nhiên để xác thực email
             string activationToken = Guid.NewGuid().ToString();
             try
             {
                 TempData["Result"] = "";
 
+                // Xử lí upload file từ client lên server IEnumerable<HttpPostedFileBase>
                 if (ModelState.IsValid)
                 {
+                    // Xử lý multifile upload minh chứng mầm non
                     foreach (HttpPostedFileBase file in HocSinh_MinhChungMN)
                     {
                         //Kiểm tra tập tin có sẵn để lưu.  
@@ -78,6 +81,7 @@ namespace HDU_AppXetTuyen.Controllers
                             lienCapTieuHoc.HocSinh_MinhChungMN += fileUrl + "#";
                         }
                     }
+                    // Xử lý multifile upload minh chứng giấy khai sinh
                     foreach (HttpPostedFileBase file in HocSinh_MinhChungGiayKS)
                     {
                         //Kiểm tra tập tin có sẵn để lưu.  
@@ -94,6 +98,7 @@ namespace HDU_AppXetTuyen.Controllers
 
                         }
                     }
+                    // Xử lý multifile upload minh chứng mã định danh cá nhân
                     foreach (HttpPostedFileBase file in HocSinh_MinhChungMaDinhDanh)
                     {
                         //Kiểm tra tập tin có sẵn để lưu.  
@@ -112,6 +117,7 @@ namespace HDU_AppXetTuyen.Controllers
                         }
 
                     }
+                    // Xử lý multifile upload minh chứng giấy ưu tiên
                     foreach (HttpPostedFileBase file in HocSinh_GiayUuTien)
                     {
                         //Kiểm tra tập tin có sẵn để lưu.  
@@ -127,6 +133,7 @@ namespace HDU_AppXetTuyen.Controllers
                             lienCapTieuHoc.HocSinh_GiayUuTien += fileUrl + "#";
                         }
                     }
+                    // Xử lý multifile upload minh chứng lệ phí xét tuyển
                     foreach (HttpPostedFileBase file in HocSinh_MinhChungLePhi)
                     {
                         //Kiểm tra tập tin có sẵn để lưu.  
@@ -143,24 +150,29 @@ namespace HDU_AppXetTuyen.Controllers
                         }
                     }
                 }
-                lienCapTieuHoc.HocSinh_GhiChu = "";
+                // Xử lý khi không có file và xử lí file name chứa System.Web.HttpPostedFileWrapper ở đầu chuỗi
                 lienCapTieuHoc.HocSinh_MinhChungMN = lienCapTieuHoc.HocSinh_MinhChungMN != null ? lienCapTieuHoc.HocSinh_MinhChungMN.ToString().Replace("System.Web.HttpPostedFileWrapper", "") : "";
                 lienCapTieuHoc.HocSinh_MinhChungGiayKS = lienCapTieuHoc.HocSinh_MinhChungGiayKS != null ? lienCapTieuHoc.HocSinh_MinhChungGiayKS.ToString().Replace("System.Web.HttpPostedFileWrapper", "") : "";
                 lienCapTieuHoc.HocSinh_MinhChungMaDinhDanh = lienCapTieuHoc.HocSinh_MinhChungMaDinhDanh != null ? lienCapTieuHoc.HocSinh_MinhChungMaDinhDanh.ToString().Replace("System.Web.HttpPostedFileWrapper", "") : "";
                 lienCapTieuHoc.HocSinh_GiayUuTien = lienCapTieuHoc.HocSinh_GiayUuTien != null ? lienCapTieuHoc.HocSinh_GiayUuTien.ToString().Replace("System.Web.HttpPostedFileWrapper", "") : "";
                 lienCapTieuHoc.HocSinh_MinhChungLePhi = lienCapTieuHoc.HocSinh_MinhChungLePhi != null ? lienCapTieuHoc.HocSinh_MinhChungLePhi.ToString().Replace("System.Web.HttpPostedFileWrapper", "") : "";
+                
+                // Mã code tự sinh để xác thực email
                 lienCapTieuHoc.HocSinh_Activation = activationToken;
+                lienCapTieuHoc.HocSinh_GhiChu = "";
 
                 db.LienCapTieuHocs.Add(lienCapTieuHoc);
                 db.SaveChanges();
                 TempData["Result"] = "THANHCONG";
-                ViewBag.status = "Thành công, Đăng ký thông tin thành công! Vui lòng xác thực lại thông tin đã gửi đến email";
+                var gioitinhHocSinh = lienCapTieuHoc.HocSinh_GioiTinh == 0 ? "Nam" : "Nữ";
+                // Gửi mail xác thực
                 string activationUrl = Url.Action("ConfirmInfomation", "LienCapTieuHocs", new { token = activationToken }, Request.Url.Scheme);
-                var subject = "Xác nhận thông tin đăng ký";
+                var subject = "Xác nhận thông tin đăng ký xét tuyển lớp 1";
                 var body = "Xin chào " + lienCapTieuHoc.HocSinh_HoTen + ", <br/> Bạn vừa đăng ký dự tuyển vào lớp 1 trường TH, THCS & THPT Hồng Đức. Vui lòng xác nhận lại các thông tin sau: " +
 
                      "<p> Họ và tên: " + lienCapTieuHoc.HocSinh_HoTen + "</p>" +
                      "<p> Ngày tháng năm sinh: " + lienCapTieuHoc.HocSinh_NgaySinh + "</p>" +
+                     "<p> Giới tính: " + gioitinhHocSinh + "</p>" +
                      "<p> Nơi sinh: " + lienCapTieuHoc.HocSinh_NoiSinh + "</p>" +
                      "<p> Nơi cư trú: " + lienCapTieuHoc.HocSinh_NoiCuTru + "</p>" +
                      "<p> Họ và tên cha: " + lienCapTieuHoc.HocSinh_ThongTinCha + "</p>" +
@@ -170,79 +182,26 @@ namespace HDU_AppXetTuyen.Controllers
                      "<p>Nếu thông tin chính xác, vui lòng ấn vào đường link sau để xác nhận: " + activationUrl + " </p>";
 
                 SendEmail(lienCapTieuHoc.HocSinh_Email, body, subject);
-                return View();
+                return RedirectToAction("ConfirmRegister", new { token = activationToken });
             }
             catch (Exception e)
             {
                 TempData["Result"] = "THAIBAI";
                 System.Diagnostics.Debug.WriteLine(e);
             }
+            TempData["Result"] = "THAIBAI";
             return View();
         }
-
-        // GET: LienCapTieuHocs/Edit/5
-        public ActionResult Edit(long? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            LienCapTieuHoc lienCapTieuHoc = db.LienCapTieuHocs.Find(id);
-            if (lienCapTieuHoc == null)
-            {
-                return HttpNotFound();
-            }
-            return View(lienCapTieuHoc);
-        }
-
-        // POST: LienCapTieuHocs/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "HocSinh_ID,HocSinh_DinhDanh,HocSinh_HoTen,HocSinh_GioiTinh,HocSinh_NgaySinh,HocSinh_NoiSinh,HocSinh_Email,HocSinh_NoiCuTru,HocSinh_TruongMN,HocSinh_UuTien,HocSinh_ThongTinCha,HocSinh_NgheNghiepCha,HocSinh_DienThoaiCha,HocSinh_ThongTinMe,HocSinh_NgheNghiepMe,HocSinh_DienThoaiMe,HocSinh_MinhChungMN,HocSinh_MinhChungGiayKS,HocSinh_MinhChungMaDinhDanh,HocSinh_GiayUuTien,HocSinh_XacNhanLePhi,HocSinh_TrangThai,HocSinh_GhiChu")] LienCapTieuHoc lienCapTieuHoc)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(lienCapTieuHoc).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(lienCapTieuHoc);
-        }
-
-        // GET: LienCapTieuHocs/Delete/5
-        public ActionResult Delete(long? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            LienCapTieuHoc lienCapTieuHoc = db.LienCapTieuHocs.Find(id);
-            if (lienCapTieuHoc == null)
-            {
-                return HttpNotFound();
-            }
-            return View(lienCapTieuHoc);
-        }
-
-        // POST: LienCapTieuHocs/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(long id)
-        {
-            LienCapTieuHoc lienCapTieuHoc = db.LienCapTieuHocs.Find(id);
-            db.LienCapTieuHocs.Remove(lienCapTieuHoc);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
         public ActionResult ConfirmInfomation()
         {
             return View();
         }
-
+        public ActionResult ConfirmRegister()
+        {
+            return View();
+        }
         [HttpPost]
+        // Sử dụng để xác thựcqua EMAIL
         public JsonResult ConfirmInfomation(string token)
         {
             var hocSinh = db.LienCapTieuHocs.Where(n => n.HocSinh_Activation == token).FirstOrDefault();
@@ -256,6 +215,17 @@ namespace HDU_AppXetTuyen.Controllers
             return Json(new { success = false });
         }
 
+        // Sử dụng để xác thực khi gửi thông tin đăng ký thành công
+        [HttpPost]
+        public JsonResult ConfirmRegister(string token)
+        {
+            var hocSinh = db.LienCapTieuHocs.Where(n => n.HocSinh_Activation == token).FirstOrDefault();
+            if (hocSinh != null)
+            {
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
         private void SendEmail(string email, string body, string subject)
         {
             using (MailMessage mm = new MailMessage("xettuyen@hdu.edu.vn", email))
