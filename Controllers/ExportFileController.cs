2b1e8f8eb72f70e44b347082f8644077eab10223
Cuongyd196 <Cuongyd196@gmail.com>
2023-07-07 12:03:35 +0700

    #update export file

--- Controllers/ExportFileController.cs
+++ Controllers/ExportFileController.cs
@@ -12,6 +12,8 @@ using System.Web.Script.Serialization;
 using Xceed.Document.NET;
 using Xceed.Words.NET;
 using HDU_AppXetTuyen.Ultils;
+using System.Web.UI.WebControls;
+
 namespace HDU_AppXetTuyen.Controllers
 {
     public class ExportFileController : Controller
@@ -152,6 +154,12 @@ namespace HDU_AppXetTuyen.Controllers
                     table.MergeCellsInColumn(0, index, index + 2);
                     table.MergeCellsInColumn(1, index, index + 2);
                     table.MergeCellsInColumn(2, index, index + 2);
+
+                    // Căn giữa theo chiều dọc cell
+
+                    table.Rows[index].Cells[0].VerticalAlignment = VerticalAlignment.Center; ;
+                    table.Rows[index].Cells[1].VerticalAlignment = VerticalAlignment.Center; ;
+                    table.Rows[index].Cells[2].VerticalAlignment = VerticalAlignment.Center; ;
 
                     // Tăng giá trị index để fill nguyện vọng tiếp theo
                     index = index + 3;
@@ -488,7 +496,7 @@ namespace HDU_AppXetTuyen.Controllers
             // Từ idDkxt lấy ra thông tin thí sinh
             var thiSinhInfo = db.ThiSinhDangKies.Find(idThiSinhInt);
             // Từ id thí sinh lấy ra tất cả nguyện vọng đăng ký xét tuyển của thí sinh
-            var listDkxt = db.DangKyXetTuyens.Where(x => x.ThiSinh_ID == idThiSinhInt).OrderBy(x => x.Dkxt_NguyenVong).ToArray();
+            var listDkxt = db.DangKyXetTuyenKQTQGs.Where(x => x.ThiSinh_ID == idThiSinhInt).OrderBy(x => x.Dkxt_KQTQG_NguyenVong).ToArray();
 
             int idKhuVucTS = (int)thiSinhInfo.KhuVuc_ID;
             // Từ khu vực id lấy ra tên khu vực
@@ -517,8 +525,8 @@ namespace HDU_AppXetTuyen.Controllers
                 document.ReplaceText("<<ThiSinh_DoiTuong>>", tenDoiTuong);
 
                 // Xử lý học lực và hạnh kiểm
-                string hocluc = getHocLucById((int)listDkxt[0].Dkxt_XepLoaiHocLuc_12);
-                string hanhkiem = getHanhKiemById((int)listDkxt[0].Dkxt_XepLoaiHanhKiem_12);
+                string hocluc = getHocLucById((int)listDkxt[0].Dkxt_KQTQG_XepLoaiHocLuc_12);
+                string hanhkiem = getHanhKiemById((int)listDkxt[0].Dkxt_KQTQG_XepLoaiHanhKiem_12);
                 document.ReplaceText("<<ThiSinh_HocLuc12>>", hocluc);
                 document.ReplaceText("<<ThiSinh_HanhKiem12>>", hanhkiem);
 
@@ -534,66 +542,53 @@ namespace HDU_AppXetTuyen.Controllers
                     var tenNganh = db.Nganhs.Find(item.Nganh_ID).NganhTenNganh;
                     var maNganh = db.Nganhs.Find(item.Nganh_ID).Nganh_MaNganh;
 
-                    // string to obj   
-                    var jsonStringMon1 = JsonConvert.SerializeObject(item.Dkxt_Diem_M1);
-                    var jsonStringMon2 = JsonConvert.SerializeObject(item.Dkxt_Diem_M2);
-                    var jsonStringMon3 = JsonConvert.SerializeObject(item.Dkxt_Diem_M3);
-
-                    DiemMon1 diemmon1 = JsonConvert.DeserializeObject<DiemMon1>(item.Dkxt_Diem_M1);
-                    DiemMon2 diemmon2 = JsonConvert.DeserializeObject<DiemMon2>(item.Dkxt_Diem_M2);
-                    DiemMon3 diemmon3 = JsonConvert.DeserializeObject<DiemMon3>(item.Dkxt_Diem_M3);
+                    DiemThiGQMon1 diemmon1 = JsonConvert.DeserializeObject<DiemThiGQMon1>(item.Dkxt_KQTQG_Diem_M1);
+                    DiemThiGQMon2 diemmon2 = JsonConvert.DeserializeObject<DiemThiGQMon2>(item.Dkxt_KQTQG_Diem_M2);
+                    DiemThiGQMon3 diemmon3 = JsonConvert.DeserializeObject<DiemThiGQMon3>(item.Dkxt_KQTQG_Diem_M3);
 
                     // Lấy ra thông tin chi tiết của từng môn1
                     var mon1_tenmon = diemmon1.TenMon1.ToString();
-                    var mon1_hk1 = diemmon1.HK1.ToString();
-                    var mon1_hk2 = diemmon1.HK2.ToString();
-                    var mon1_hk3 = diemmon1.HK3.ToString();
-                    var mon1_diemtb = diemmon1.DiemTrungBinh != null ? diemmon1.DiemTrungBinh.ToString() : " ";
-
+                    var mon1_diem = diemmon1.DiemM1.ToString();
                     // Lấy ra thông tin chi tiết của từng môn2
                     var mon2_tenmon = diemmon2.TenMon2.ToString();
-                    var mon2_hk1 = diemmon2.HK1.ToString();
-                    var mon2_hk2 = diemmon2.HK2.ToString();
-                    var mon2_hk3 = diemmon2.HK3.ToString();
-                    var mon2_diemtb = diemmon2.DiemTrungBinh != null ? diemmon2.DiemTrungBinh.ToString() : " ";
-
+                    var mon2_diem = diemmon2.DiemM2.ToString();
                     // Lấy ra thông tin chi tiết của từng môn3
                     var mon3_tenmon = diemmon3.TenMon3.ToString();
-                    var mon3_hk1 = diemmon3.HK1.ToString();
-                    var mon3_hk2 = diemmon3.HK2.ToString();
-                    var mon3_hk3 = diemmon3.HK3.ToString();
-                    var mon3_diemtb = diemmon3.DiemTrungBinh != null ? diemmon3.DiemTrungBinh.ToString() : " ";
+                    var mon3_diem = diemmon3.DiemM3.ToString();
+
+                    var tongdiem = item.Dkxt_KQTQG_Diem_Tong;
 
                     // 1 nguyện vọng chèn 3 row
                     table.InsertRow();
                     table.InsertRow();
                     table.InsertRow();
                     // Thêm dữ liệu vào table
-                    table.Rows[index].Cells[0].Paragraphs[0].Append(item.Dkxt_NguyenVong.ToString()).Font("Times New Roman").Alignment = Alignment.center;
+                    table.Rows[index].Cells[0].Paragraphs[0].Append(item.Dkxt_KQTQG_NguyenVong.ToString()).Font("Times New Roman").Alignment = Alignment.center;
+                    table.Rows[index].Cells[0].VerticalAlignment = VerticalAlignment.Center; ;
+
                     table.Rows[index].Cells[1].Paragraphs[0].Append(tenNganh).Font("Times New Roman");
+                    table.Rows[index].Cells[1].VerticalAlignment = VerticalAlignment.Center; ;
+
                     table.Rows[index].Cells[2].Paragraphs[0].Append(maNganh).Font("Times New Roman").Alignment = Alignment.center;
+                    table.Rows[index].Cells[2].VerticalAlignment = VerticalAlignment.Center; ;
+
                     table.Rows[index].Cells[3].Paragraphs[0].Append("Môn 1:" + mon1_tenmon).Font("Times New Roman");
-                    table.Rows[index].Cells[4].Paragraphs[0].Append(mon1_hk1).Font("Times New Roman").Alignment = Alignment.center;
-                    table.Rows[index].Cells[5].Paragraphs[0].Append(mon1_hk2).Font("Times New Roman").Alignment = Alignment.center;
-                    table.Rows[index].Cells[6].Paragraphs[0].Append(mon1_hk3).Font("Times New Roman").Alignment = Alignment.center;
-                    table.Rows[index].Cells[7].Paragraphs[0].Append(mon1_diemtb).Font("Times New Roman").Alignment = Alignment.center;
+                    table.Rows[index].Cells[4].Paragraphs[0].Append(mon1_diem).Font("Times New Roman").Alignment = Alignment.center;
+                    table.Rows[index].Cells[5].Paragraphs[0].Append(tongdiem).Font("Times New Roman").Alignment = Alignment.center;
+                    table.Rows[index].Cells[5].VerticalAlignment = VerticalAlignment.Center; ;
+
 
                     table.Rows[index + 1].Cells[3].Paragraphs[0].Append("Môn 2: " + mon2_tenmon).Font("Times New Roman");
-                    table.Rows[index + 1].Cells[4].Paragraphs[0].Append(mon2_hk1).Font("Times New Roman").Alignment = Alignment.center;
-                    table.Rows[index + 1].Cells[5].Paragraphs[0].Append(mon2_hk2).Font("Times New Roman").Alignment = Alignment.center;
-                    table.Rows[index + 1].Cells[6].Paragraphs[0].Append(mon2_hk3).Font("Times New Roman").Alignment = Alignment.center;
-                    table.Rows[index + 1].Cells[7].Paragraphs[0].Append(mon2_diemtb).Font("Times New Roman").Alignment = Alignment.center;
+                    table.Rows[index + 1].Cells[4].Paragraphs[0].Append(mon2_diem).Font("Times New Roman").Alignment = Alignment.center;
 
                     table.Rows[index + 2].Cells[3].Paragraphs[0].Append("Môn 3: " + mon3_tenmon).Font("Times New Roman");
-                    table.Rows[index + 2].Cells[4].Paragraphs[0].Append(mon3_hk1).Font("Times New Roman").Alignment = Alignment.center;
-                    table.Rows[index + 2].Cells[5].Paragraphs[0].Append(mon3_hk2).Font("Times New Roman").Alignment = Alignment.center;
-                    table.Rows[index + 2].Cells[6].Paragraphs[0].Append(mon3_hk3).Font("Times New Roman").Alignment = Alignment.center;
-                    table.Rows[index + 2].Cells[7].Paragraphs[0].Append(mon3_diemtb).Font("Times New Roman").Alignment = Alignment.center;
+                    table.Rows[index + 2].Cells[4].Paragraphs[0].Append(mon3_diem).Font("Times New Roman").Alignment = Alignment.center;
 
                     // Merge đúng định dạng
                     table.MergeCellsInColumn(0, index, index + 2);
                     table.MergeCellsInColumn(1, index, index + 2);
                     table.MergeCellsInColumn(2, index, index + 2);
+                    table.MergeCellsInColumn(5, index, index + 2);
 
                     // Tăng giá trị index để fill nguyện vọng tiếp theo
                     index = index + 3;
@@ -612,6 +607,7 @@ namespace HDU_AppXetTuyen.Controllers
             }
 
         }
+
         public string getHocLucById(int id)
         {
             switch (id)
