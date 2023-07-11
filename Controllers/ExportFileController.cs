4b04c619c9a6082571fa85ec5ae13496af6bbb31
Cuongyd196 <cuongyd196@gmail.com>
2023-07-11 16:08:37 +0700

    #update xuat phieu tong hop

--- Controllers/ExportFileController.cs
+++ Controllers/ExportFileController.cs
@@ -13,6 +13,7 @@ using Xceed.Document.NET;
 using Xceed.Words.NET;
 using HDU_AppXetTuyen.Ultils;
 using System.Web.UI.WebControls;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
 
 namespace HDU_AppXetTuyen.Controllers
 {
@@ -25,10 +26,8 @@ namespace HDU_AppXetTuyen.Controllers
         {
             return View();
         }
-        public ActionResult DownloadFile_XetTuyenHB(string idThiSinh)
+        public ActionResult DownloadFile_XetTuyenHB()
         {
-            // Không cần sử dụng string idThiSinh vì lấy id Thí sinh từ session
-
             var idThiSinhInt = 0;
             // Check login session có tồn tại hay không nếu không tồn tại thì FIX idThiSinhInt = 2
             if (Session["login_session"] == null)
@@ -75,8 +74,9 @@ namespace HDU_AppXetTuyen.Controllers
                 document.ReplaceText("<<ThiSinh_DienThoai>>", thiSinhInfo.ThiSinh_DienThoai);
                 document.ReplaceText("<<ThiSinh_KhuVuc>>", tenKhuVuc);
                 document.ReplaceText("<<ThiSinh_DoiTuong>>", tenDoiTuong);
+                document.ReplaceText("<<ThiSinh_DCNhanGiayBao>>", thiSinhInfo.ThiSinh_DCNhanGiayBao);
 
-                 // Xử lý học lực và hạnh kiểm
+                // Xử lý học lực và hạnh kiểm
                 string hocluc = getHocLucById((int)listDkxt[0].Dkxt_XepLoaiHocLuc_12);
                 string hanhkiem = getHanhKiemById((int)listDkxt[0].Dkxt_XepLoaiHanhKiem_12);
                 document.ReplaceText("<<ThiSinh_HocLuc12>>", hocluc);
@@ -178,9 +178,8 @@ namespace HDU_AppXetTuyen.Controllers
             }
            
         }
-        public ActionResult DownloadFile_XetTuyenThang(string idThiSinh)
+        public ActionResult DownloadFile_XetTuyenThang()
         {
-            // Không cần sử dụng string idThiSinh vì lấy id Thí sinh từ session
 
             var idThiSinhInt = 0;
             // Check login session có tồn tại hay không nếu không tồn tại thì FIX idThiSinhInt = 2 - DEV
@@ -228,6 +227,7 @@ namespace HDU_AppXetTuyen.Controllers
                 document.ReplaceText("<<ThiSinh_DienThoai>>", thiSinhInfo.ThiSinh_DienThoai);
                 document.ReplaceText("<<ThiSinh_KhuVuc>>", tenKhuVuc);
                 document.ReplaceText("<<ThiSinh_DoiTuong>>", tenDoiTuong);
+                document.ReplaceText("<<ThiSinh_DCNhanGiayBao>>", thiSinhInfo.ThiSinh_DCNhanGiayBao);
 
                 // Xử lý học lực và hạnh kiểm
                 string hocluc = getHocLucById((int)listDkxt[0].Dkxt_XepLoaiHocLuc_12);
@@ -277,9 +277,8 @@ namespace HDU_AppXetTuyen.Controllers
         }        
 
         // Xét tuyển theo chứng chỉ ngoại ngữ
-        public ActionResult DownloadFile_XetTuyenCCNN(string idThiSinh)
+        public ActionResult DownloadFile_XetTuyenCCNN()
         {
-            // Không cần sử dụng string idThiSinh vì lấy id Thí sinh từ session
 
             var idThiSinhInt = 0;
             // Check login session có tồn tại hay không nếu không tồn tại thì FIX idThiSinhInt = 2 - DEV
@@ -327,6 +326,7 @@ namespace HDU_AppXetTuyen.Controllers
                 document.ReplaceText("<<ThiSinh_DienThoai>>", thiSinhInfo.ThiSinh_DienThoai);
                 document.ReplaceText("<<ThiSinh_KhuVuc>>", tenKhuVuc);
                 document.ReplaceText("<<ThiSinh_DoiTuong>>", tenDoiTuong);
+                document.ReplaceText("<<ThiSinh_DCNhanGiayBao>>", thiSinhInfo.ThiSinh_DCNhanGiayBao);
 
                 // Xử lý học lực và hạnh kiểm
                 string hocluc = getHocLucById((int)listDkxt[0].Dkxt_XepLoaiHocLuc_12);
@@ -376,9 +376,8 @@ namespace HDU_AppXetTuyen.Controllers
         }     
         
         // Xét tuyển đánh giá năng lực đánh giá tư duy
-        public ActionResult DownloadFile_XetTuyenDGNL_DGTD(string idThiSinh)
+        public ActionResult DownloadFile_XetTuyenDGNL_DGTD()
         {
-            // Không cần sử dụng string idThiSinh vì lấy id Thí sinh từ session
 
             var idThiSinhInt = 0;
             // Check login session có tồn tại hay không nếu không tồn tại thì FIX idThiSinhInt = 2 - DEV
@@ -426,6 +425,7 @@ namespace HDU_AppXetTuyen.Controllers
                 document.ReplaceText("<<ThiSinh_DienThoai>>", thiSinhInfo.ThiSinh_DienThoai);
                 document.ReplaceText("<<ThiSinh_KhuVuc>>", tenKhuVuc);
                 document.ReplaceText("<<ThiSinh_DoiTuong>>", tenDoiTuong);
+                document.ReplaceText("<<ThiSinh_DCNhanGiayBao>>", thiSinhInfo.ThiSinh_DCNhanGiayBao);
 
                 // Xử lý học lực và hạnh kiểm
                 string hocluc = getHocLucById((int)listDkxt[0].Dkxt_XepLoaiHocLuc_12);
@@ -473,9 +473,8 @@ namespace HDU_AppXetTuyen.Controllers
             }
 
         }
-        public ActionResult DownloadFile_XetTuyenKQTQG(string idThiSinh)
+        public ActionResult DownloadFile_XetTuyenKQTQG()
         {
-            // Không cần sử dụng string idThiSinh vì lấy id Thí sinh từ session
 
             var idThiSinhInt = 0;
             // Check login session có tồn tại hay không nếu không tồn tại thì FIX idThiSinhInt = 2
@@ -523,6 +522,7 @@ namespace HDU_AppXetTuyen.Controllers
                 document.ReplaceText("<<ThiSinh_DienThoai>>", thiSinhInfo.ThiSinh_DienThoai);
                 document.ReplaceText("<<ThiSinh_KhuVuc>>", tenKhuVuc);
                 document.ReplaceText("<<ThiSinh_DoiTuong>>", tenDoiTuong);
+                document.ReplaceText("<<ThiSinh_DCNhanGiayBao>>", thiSinhInfo.ThiSinh_DCNhanGiayBao);
 
                 // Xử lý học lực và hạnh kiểm
                 string hocluc = getHocLucById((int)listDkxt[0].Dkxt_KQTQG_XepLoaiHocLuc_12);
@@ -596,6 +596,94 @@ namespace HDU_AppXetTuyen.Controllers
                 }
                 // Generate a unique file name
                 string fileName = thiSinhInfo.ThiSinh_Ten + "_bieu-mau-xet-thpt-2021-2022_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".docx";
+
+                // Save the filled document to a temporary location on the server
+                string tempFilePath = Path.Combine(Path.GetTempPath(), fileName);
+                document.SaveAs(tempFilePath);
+
+                // Return the file for download
+                byte[] fileBytes = System.IO.File.ReadAllBytes(tempFilePath);
+                return File(fileBytes, "application/vnd.openxmlformats-officedocument.wordprocessingml.document", fileName);
+            }
+
+        }
+        
+        // Xét tuyển thi năng khiếu
+        public ActionResult DownloadFile_XetTuyenThiNangKhieu()
+        {
+            var idThiSinhInt = 0;
+            // Check login session có tồn tại hay không nếu không tồn tại thì FIX idThiSinhInt = 2
+            if (Session["login_session"] == null)
+            {
+                idThiSinhInt = 2;
+                System.Diagnostics.Debug.WriteLine("login_session is NULL: ");
+            }
+            else
+            {
+                string str_thisinh_session = Session["login_session"].ToString();
+                var thisinh_session = db.ThiSinhDangKies.Where(x => x.ThiSinh_MatKhau == str_thisinh_session).FirstOrDefault();
+                idThiSinhInt = (int)thisinh_session.ThiSinh_ID;
+                System.Diagnostics.Debug.WriteLine("idThiSinhInt: " + idThiSinhInt);
+            }
+
+            string templateFilePath = Server.MapPath("~/Content/static/export-66-bieu-mau-thi-nk-20230302101245-e.docx");
+            // Từ idDkxt lấy ra thông tin thí sinh
+            var thiSinhInfo = db.ThiSinhDangKies.Find(idThiSinhInt);
+            // Từ id thí sinh lấy ra nguyện vọng 1 của thí sinh
+            var nv1_dkxt = db.DangKyDuThiNangKhieus.Where(x => x.ThiSinh_ID == idThiSinhInt && x.Dkdt_NK_NguyenVong == 1);
+
+            // từ id ngành lấy ra mã ngành và tên ngành
+            var maNganh = db.Nganhs.Where(x => x.Nganh_ID == nv1_dkxt.FirstOrDefault().Nganh_ID).FirstOrDefault().Nganh_MaNganh;
+
+            using (DocX document = DocX.Load(templateFilePath))
+            {
+                // Replace placeholders with actual data
+                document.ReplaceText("<<ThiSinh_HoLot>>", thiSinhInfo.ThiSinh_HoLot);
+                document.ReplaceText("<<ThiSinh_Ten>>", thiSinhInfo.ThiSinh_Ten);
+                document.ReplaceText("<<ThiSinh_CCCD>>", thiSinhInfo.ThiSinh_CCCD);
+                document.ReplaceText("<<ThiSinh_NgaySinh>>", thiSinhInfo.ThiSinh_NgaySinh);
+                document.ReplaceText("<<ThiSinh_DienThoai>>", thiSinhInfo.ThiSinh_DienThoai);
+                document.ReplaceText("<<ThiSinh_DCNhanGiayBao>>", thiSinhInfo.ThiSinh_DCNhanGiayBao);
+                document.ReplaceText("<<ThiSinh_Email>>", thiSinhInfo.ThiSinh_Email);
+                document.ReplaceText("<<ThiSinh_Email>>", thiSinhInfo.ThiSinh_Email);
+
+                //Nganh_ID	Nganh_MaNganh	NganhTenNganh	Khoa_ID	Nganh_GhiChu
+                //4    7140201 Giáo dục Mầm non    5   7140201 Giáo dục Mầm non
+                // 5   7140206 Giáo dục thể chất   11  7140206 Giáo dục thể chất
+                // 6   7140202 Giáo dục Tiểu học   4   7140202 Giáo dục Tiểu học
+                //Ngành sử dụng môn thi năng khiếu để xét tuyển vào(đánh dấu X vào ô trống):
+                var checkbox_x = "x";
+                var xt_cb_mn = " ";
+                var xt_cb_th = " ";
+                var xt_cb_tc = " ";
+                var thi_cb_mnth = " ";
+                var thi_cb_tc = " ";
+
+                if (maNganh == "7140201")
+                {
+                    xt_cb_mn = checkbox_x;
+                    thi_cb_mnth = checkbox_x;
+                }
+                else if(maNganh == "7140202")
+                {
+                    xt_cb_th = checkbox_x;
+                    thi_cb_mnth = checkbox_x;
+                }
+                else if(maNganh == "7140206")
+                {
+                    xt_cb_tc = checkbox_x;
+                    thi_cb_tc = checkbox_x;
+                }
+                document.ReplaceText("<<xt_cb_mn>>", xt_cb_mn);
+                document.ReplaceText("<<xt_cb_th>>", xt_cb_th);
+                document.ReplaceText("<<xt_cb_tc>>", xt_cb_tc);
+
+                //Đăng kí môn thi năng khiếu(chỉ đánh dấu X vào 1 ô trống):
+                document.ReplaceText("<<thi_cb_mnth>>", thi_cb_mnth);
+                document.ReplaceText("<<thi_cb_tc>>", thi_cb_tc);
+
+                // Generate a unique file name
+                string fileName = thiSinhInfo.ThiSinh_Ten + "_bieu-mau-thi-nk_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".docx";
 
                 // Save the filled document to a temporary location on the server
                 string tempFilePath = Path.Combine(Path.GetTempPath(), fileName);
